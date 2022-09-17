32a2a28cdbeb0a443b30934512ca6c8861652e4b
Marcos Frank <Marcos_Frank@hotmail.com>
2022-09-17 11:58:04 -0300

    Se cambiaron comentarios de ingles a español

--- NetCoreCourse/NetCoreCourse.FirstExample.WebApp/Program.cs
+++ NetCoreCourse/NetCoreCourse.FirstExample.WebApp/Program.cs
@@ -1,16 +1,18 @@
-// Don't we have a class? Let's take a look with a Decompiler. 
+// No tenemos una clase. Como es posible? Utilicemos un decompilador.
+using Microsoft.EntityFrameworkCore;
 using NetCoreCourse.FirstExample.WebApp.Configuration;
+using NetCoreCourse.FirstExample.WebApp.DataAccess;
 using NetCoreCourse.FirstExample.WebApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-// Add services to the container.
+//Agregamos las paginas de Razon. Que son? Las vamos a ver en el modulo de MVC.
 builder.Services.AddRazorPages();
 
-//Adding first configuration object.
+//Agregando el primer objeto de configuracion.
 var firstConfigurationObject = builder.Configuration.GetSection("FirstConfiguration");
 builder.Services.Configure<FirstConfigurationOptions>(firstConfigurationObject);
 
-//Dependency Injection
+// Agregamos los servicios al contenedor de dependencias
 builder.Services.AddTransient<IForecastService, ForecastService>();
 builder.Services.AddTransient<IServiceUsingServices, ServiceUsingServices>();
 
@@ -18,20 +20,28 @@ builder.Services.AddTransient<ITransientRandomValueService, RandomValueService>(
 builder.Services.AddScoped<IScopedRandomValueService, RandomValueService>();
 builder.Services.AddSingleton<ISingletonRandomValueService, RandomValueService>();
 
-//Building App
+//Lo vamos a ver en el Modulo de EF Core
+//builder.Services.AddDbContext<ThingsContext>(options =>
+//{
+//    //Para poder utilizar SqlServer necesitamos instalar el paquete
+//    //Microsoft.EntityFrameworkCore.SqlServer
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("ThingsContextConnection"));
+//});
+
+//Creando la aplicacion.
 var app = builder.Build();
 
-// Configure the HTTP request pipelines. MIDDLEWARES.
+// Configurando el "pipeline" para las peticiones "HTTP". MIDDLEWARES.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
 }
-//Adding a New Custom Environment
+//Probando nuevos ambientes.
 if (app.Environment.IsEnvironment("MarcosDev"))
 {
     app.Logger.LogInformation("Este es el ambiente de Marcos.");
 }
-//TODO Check this ---> Funciona pero ver la diferencia entre uno y el otro
+//TODO TODO TODO Check this ---> Funciona pero ver la diferencia entre uno y el otro
 //app.UseHsts(); // It returns a header that tells the browser to always try to do the first request over HTTPS.
 //app.UseHttpsRedirection();
 
@@ -42,7 +52,7 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
-//Minimal API Definition. More info on: https://docs.microsoft.com/en-us/aspnet/core/fundamentals/minimal-apis?view=aspnetcore-6.0
+//Definicion de "Minimal API". Mas info en: https://docs.microsoft.com/en-us/aspnet/core/fundamentals/minimal-apis?view=aspnetcore-6.0
 app.MapGet("/api/firstapi", () => "Hey here is your first API!");
 
 app.MapControllerRoute(
