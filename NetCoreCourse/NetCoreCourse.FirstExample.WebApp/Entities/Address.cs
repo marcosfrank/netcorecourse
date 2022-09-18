29ad14350e63db8029f5f9db2eb91fec8feaa8fc
Marcos Frank <Marcos_Frank@hotmail.com>
2022-09-18 19:36:01 -0300

    Cambios para mostrar como se genera una migracion en las clases teoricas.

--- NetCoreCourse/NetCoreCourse.FirstExample.WebApp/Entities/Address.cs
+++ NetCoreCourse/NetCoreCourse.FirstExample.WebApp/Entities/Address.cs
@@ -6,5 +6,8 @@
     public class Address
     {
         public int Id { get; set; }
+        //public string Street { get; set; }
+        //public string City { get; set; }
+        //public string Number { get; set; }    
     }
 }
